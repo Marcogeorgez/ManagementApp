be8bb167e857872b61ee08e35efcf5db0c32ee1d
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-11-13 08:49:17 +0200

    changed working month in db to not required because when client create a project he can't set the working month.

---
+++ Data/Migrations/20241113064847_workingMonthNOTRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuminaryVisuals.Migrations
+{
+    /// <inheritdoc />
+    public partial class workingMonthNOTRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
