38180af78e9408717ae8d254f4d9e407510f0365
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-11-13 16:36:15 +0200

    Added setting table to db along with setting service to create/update/read the settings

---
+++ Data/Migrations/20241113132055_CreatingSettingToStoreSettingWideValues.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LuminaryVisuals.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingSettingToStoreSettingWideValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ConversionRateUSToLek = table.Column<decimal>(type: "numeric", nullable: false),
+                    UpdatedByUserId = table.Column<string>(type: "character varying(255)", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Settings_Users_UpdatedByUserId",
+                        column: x => x.UpdatedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Settings_UpdatedByUserId",
+                table: "Settings",
+                column: "UpdatedByUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
