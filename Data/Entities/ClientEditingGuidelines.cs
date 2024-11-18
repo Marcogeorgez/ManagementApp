b0ec5dec977b62ff86ef18a7df33022f71ffaa73
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-11-18 16:49:38 +0200

    fully implemented client editing guidelines UI and Entity along with saving it in database.

--- Data/Entities/ClientEditingGuidelines.cs
+++ Data/Entities/ClientEditingGuidelines.cs
@@ -13,45 +13,27 @@ namespace LuminaryVisuals.Data.Entities
         [ForeignKey(nameof(UserId))]
         public virtual ApplicationUser User { get; set; } 
         public string? WebsiteLink { get; set; }
-
         public string? VideoStructure { get; set; } 
-
         public string? CrossFades { get; set; } 
-
         public string? FadeToBlack { get; set; } 
-
         public string? BlackAndWhite { get; set; } 
-
         public string? DoubleExposure { get; set; } 
-
         public string? MaskingTransitions { get; set; } 
-
         public string? LensFlares { get; set; } 
-
         public string? OldFilmLook { get; set; } 
-
         public string? PictureInPicture { get; set; }
-
         public string? OtherTransitions { get; set; } 
         public string? TransitionComments { get; set; } 
-
         public string? UseSpeeches { get; set; } 
-
         public string? SpeechComments { get; set; }
-
         public string? SoundDesignEmphasis { get; set; } 
         public string? SoundDesignComments { get; set; } 
-
-        public string? MusicGenresArtists { get; set; }
-
+        public string? musicLicensingSites { get; set; }
         public string? SongSamples { get; set; }
-
         public string? ColorReferences { get; set; } 
-
         public string? FilmReferences { get; set; } 
-
+        public string? ClientSamples { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
     }
