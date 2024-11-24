1ad340d048b6ea51ed37ef4b3b71f2389e8233ac
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-11-24 18:03:55 +0200

    fix: project.Client.HourlyRate instead of project.Client.HourlyRate.Value cause it may contain null fix: not saved roles changes in case of specific names

--- LuminaryVisuals/Services/ProjectService.cs
+++ LuminaryVisuals/Services/ProjectService.cs
@@ -55,7 +55,7 @@ public class ProjectService
                 project.SecondaryEditorName = project.SecondaryEditorId != null && userNames.ContainsKey(project.SecondaryEditorId)
                     ? userNames[project.SecondaryEditorId]!
                     : "No Editor Assigned";
-                if (project.ClientBillableHours != null && project.Client.HourlyRate.Value != null && project.PrimaryEditor != null)
+                if (project.ClientBillableHours != null && project.Client.HourlyRate != null && project.PrimaryEditor != null)
                 {
                     project.ClientBillableAmount = project.Client.HourlyRate.Value * project.ClientBillableHours;
                     project.EditorPaymentAmount = project.PrimaryEditor.HourlyRate * project.BillableHours;
