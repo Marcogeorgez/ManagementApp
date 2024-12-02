d9092905ec81db69a977d729524d231b8605b6f1
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-12-02 05:56:52 +0200

    restructure project by including models folder for less clutter in entities

---
+++ LuminaryVisuals/Models/ProjectStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace LuminaryVisuals.Data.Entities
+{
+    public enum ProjectStatus
+    {
+        Upcoming = 0,
+        Ready_To_Edit = 1,
+        Scheduled = 2,
+        Working = 3,
+        Review = 4,
+        Delivered = 5,
+        Revision = 6,
+        Finished = 7
+    }
+
+    public enum AdminProjectStatus
+    {
+        Not_Finished = 0,
+        Delivered_Not_Paid = 1,
+        Sent_Invoice = 2,
+        Paid = 3,
+    }
+}
