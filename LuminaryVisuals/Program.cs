1776f5aa5e494031f6521ef1c2ebfe21fcc0f8b1
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-12-05 09:02:02 +0200

    change: snackbar to show on left to be more visible, lasts 10 seconds

--- LuminaryVisuals/Program.cs
+++ LuminaryVisuals/Program.cs
@@ -49,6 +49,12 @@ builder.Services.AddMudServices(config =>
         config.SnackbarConfiguration.PreventDuplicates = false;
         config.SnackbarConfiguration.NewestOnTop = false;
         config.SnackbarConfiguration.ShowCloseIcon = true;
+
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+        config.SnackbarConfiguration.VisibleStateDuration = 10000;
+        config.SnackbarConfiguration.HideTransitionDuration = 500;
+        config.SnackbarConfiguration.ShowTransitionDuration = 500;
+        config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
     });
 
 builder.Services.AddRazorComponents()
