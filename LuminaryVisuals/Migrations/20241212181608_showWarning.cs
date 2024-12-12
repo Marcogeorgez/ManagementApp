dbd603815a8b9c8338e2b3c59e17cc311323f10d
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-12-12 20:29:33 +0200

    add: IsUrgent to show warning for due dates

---
+++ LuminaryVisuals/Migrations/20241212181608_showWarning.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuminaryVisuals.Migrations
+{
+    /// <inheritdoc />
+    public partial class showWarning : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUrgent",
+                table: "Projects",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUrgent",
+                table: "Projects");
+        }
+    }
+}
