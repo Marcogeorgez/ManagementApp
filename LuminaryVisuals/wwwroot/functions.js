62e0d4abd9955791fbf95b183ed7757b11c4a6f2
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-12-18 15:43:31 +0200

    restructure files and re-eanbled local time-display for chat

---
+++ LuminaryVisuals/wwwroot/functions.js
@@ -0,0 +1,21 @@
+﻿window.saveAsFile = (filename, base64Content) => {
+    const byteCharacters = atob(base64Content);
+    const byteNumbers = new Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'text/csv' });
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+window.getTimezoneOffset = function () {
+    return -new Date().getTimezoneOffset(); // Offset in minutes, negative for UTC-
+};
