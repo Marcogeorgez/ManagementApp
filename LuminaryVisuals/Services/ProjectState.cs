db65f43fb4e5ed8740ea04404e307539c171daaa
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-12-18 10:33:06 +0200

    changed chat to be a div isntead of mud dialog and created projectState which monitor which project has been selected to open chat based on it, along with improving ui for edit message

---
+++ LuminaryVisuals/Services/ProjectState.cs
@@ -0,0 +1,13 @@
+﻿public class ProjectState
+{
+    public Project? CurrentProject { get; private set; }
+    public event Action OnChange;
+
+    public void SetProject(Project project)
+    {
+        CurrentProject = project;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
