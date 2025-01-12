6cce658b335d41865e1f6a8ca98256aceae7b68d
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-01-12 03:11:38 +0200

    Refactor UI components and implemented notification system for client preferences, Display last update for client preferences
    Restructured `ViewClientEditingGuidelinesComponent.razor` for improved layout and organization of preferences and editing guidelines. Made the `preferences` object nullable and added properties for `clientName` and `lastUpdatedDate`.
    
    Cleaned up `EditClientEditingGuidelinesComponent.razor`.
    
    Updated `INotificationService.cs` to include methods for notifying admins of client preference changes.
    
    Implemented `ClientPreferencesUpdated` in `NotificationService.cs` to handle notifications with error handling and improved message formatting.

--- LuminaryVisuals/Services/Mail/INotificationService.cs
+++ LuminaryVisuals/Services/Mail/INotificationService.cs
@@ -1,10 +1,11 @@
-﻿
-using LuminaryVisuals.Data.Entities;
+﻿using LuminaryVisuals.Data.Entities;
 
 namespace LuminaryVisuals.Services.Mail
 {
     public interface INotificationService
     {
+        Task ClientPreferencesUpdated(ApplicationUser User, string UserId);
+        Task NewUserJoinedNoitifcation(ApplicationUser User);
         Task QueueChatNotification(Project project, Message message);
         Task QueueProjectCreationNotification(Project project);
         Task QueueStatusChangeNotification(Project project, ProjectStatus oldStatus, ProjectStatus newStatus, string updatedByUserId);
