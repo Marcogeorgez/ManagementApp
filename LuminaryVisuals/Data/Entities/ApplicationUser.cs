63310f186d88e16b7a69c001d256d4a0b00256eb
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-01-15 00:41:35 +0200

    new users get 8 weeks by default

--- LuminaryVisuals/Data/Entities/ApplicationUser.cs
+++ LuminaryVisuals/Data/Entities/ApplicationUser.cs
@@ -8,7 +8,7 @@ namespace LuminaryVisuals.Data.Entities;
 public class ApplicationUser : IdentityUser
 {
     public decimal? HourlyRate { get; set; }
-    public int? WeeksToDueDateDefault { get; set; } = 4;
+    public int? WeeksToDueDateDefault { get; set; } = 8;
 
     // Navigation properties
 
