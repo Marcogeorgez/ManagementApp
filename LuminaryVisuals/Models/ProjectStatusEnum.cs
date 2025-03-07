1ff8637933b7293fdf9d5927d9154ac81700e878
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-03-07 14:57:56 +0200

    Add Display attributes to enums and introduce AdminProjectStatus
    Added System.ComponentModel.DataAnnotations namespace. Updated ProjectStatus enum with Display attributes for Ready_To_Edit and Ready_To_Review. Introduced AdminProjectStatus enum with values and Display attributes.

--- LuminaryVisuals/Models/ProjectStatusEnum.cs
+++ LuminaryVisuals/Models/ProjectStatusEnum.cs
@@ -1,11 +1,15 @@
-﻿namespace LuminaryVisuals.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuminaryVisuals.Data.Entities
 {
     public enum ProjectStatus
     {
         Upcoming = 0,
+        [Display(Description = "Ready To Edit")]
         Ready_To_Edit = 1,
         Scheduled = 2,
         Working = 3,
+        [Display(Description = "Ready To Review")]
         Ready_To_Review = 4,
         Delivered = 5,
         Revision = 6,
@@ -14,8 +18,11 @@
 
     public enum AdminProjectStatus
     {
+        [Display(Description = "Not Finished")]
         Not_Finished = 0,
+        [Display(Description = "Delivered Not Paid")]
         Delivered_Not_Paid = 1,
+        [Display(Description = "Send Invoice")]
         Sent_Invoice = 2,
         Paid = 3,
     }
