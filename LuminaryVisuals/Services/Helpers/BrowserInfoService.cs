df23833dfd348af022403f65edccd4a475157314
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-05-04 16:58:11 +0300

    BrowserInfoService

---
+++ LuminaryVisuals/Services/Helpers/BrowserInfoService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace LuminaryVisuals.Services.Helpers;
+
+public class BrowserInfoService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public BrowserInfoService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task<string> GetUserAgentAsync()
+    {
+        return await _jsRuntime.InvokeAsync<string>("getUserAgent");
+    }
+}
