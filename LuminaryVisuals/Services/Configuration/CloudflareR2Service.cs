60c81e0c8dd6330c047671c067953edc2eae14c8
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-05-09 00:57:29 +0300

    feat: auto-grow text-fields, chat-upload accept PDFs.
    - Improved file preview with PDF support and error handling.
    - Enhanced chat message link handling and scroll logic.
    - Enabled `AutoGrow` for `MudTextField` in multiple components.
    - Refined grid layouts and added vertical alignment styles.
    - Updated file naming and URL logic in `CloudflareR2Service`.
    - Improved error handling and logging for file uploads.
    - Added styles for file and PDF-related elements.

--- LuminaryVisuals/Services/Configuration/CloudflareR2Service.cs
+++ LuminaryVisuals/Services/Configuration/CloudflareR2Service.cs
@@ -3,6 +3,8 @@ using Amazon.S3.Model;
 using LuminaryVisuals.Models;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Options;
+using System.Net;
+using System.Web;
 
 namespace LuminaryVisuals.Services.Configuration;
 public class CloudflareR2Service
@@ -25,18 +27,59 @@ public class CloudflareR2Service
         {
             _logger.LogInformation($"Started Uploading file {file.Name}");
 
+            
             // Generate a unique filename
-            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.Name)}";
+            var fileName = $"{DateTime.UtcNow.ToString("dd-HHmmss")}—{file.Name.Trim()}"?? $"{Guid.NewGuid()}{Path.GetExtension(file.Name)}";
 
+            // URL encode the file name
+            string encodedFileName = WebUtility.UrlEncode(fileName);
+
+            // After encoding, replace the encoded em dash (%E2%80%94) with the actual em dash character
+            encodedFileName = encodedFileName.Replace("%E2%80%94", "—");
             // Open file stream
             await using var fileStream = file.OpenReadStream(maxAllowedSize: 10 * 1024 * 1024); // 10MB limit
 
             var request = new PutObjectRequest
             {
                 BucketName = _settings.BucketName,
-                Key = fileName,
+                Key = encodedFileName,
                 InputStream = fileStream,
                 ContentType = file.ContentType,
+                DisablePayloadSigning = true
+            };
+
+            var response = await _s3Client.PutObjectAsync(request);
+            _logger.LogInformation("File uploaded successfully.");
+            string publicUrl;
+            // Construct public URL
+            if (_env.IsProduction())
+            {
+                publicUrl = $"https://{_settings.publicURL}/{encodedFileName}";
+            }
+            else
+            {
+                publicUrl = $"https://pub-{_settings.publicURL}.r2.dev/{encodedFileName}";
+            }
+            return publicUrl;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Logged Upload error: {ex.Message}");
+            throw;
+        }
+    }
+    public async Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType)
+    {
+        try
+        {
+            _logger.LogInformation($"Started Uploading file {fileName}");
+
+            var request = new PutObjectRequest
+            {
+                BucketName = _settings.BucketName,
+                Key = fileName,
+                InputStream = fileStream,
+                ContentType = contentType,
                 DisablePayloadSigning = true
             };
 
