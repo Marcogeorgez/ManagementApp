c892b94825001928cd99112e3d9d8933b3c1a681
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-05-10 12:22:46 +0300

    Revert Actions from dialog to menu again, Modify Project-Brief to be aligned

--- LuminaryVisuals/Program.cs
+++ LuminaryVisuals/Program.cs
@@ -346,7 +346,6 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<BrowserInfoService>();
 builder.Services.AddScoped<InvoiceService>();
 var app = builder.Build();
-
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
