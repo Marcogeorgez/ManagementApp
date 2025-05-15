b38a9ebc90664ba3a5726751f78fa717869219f3
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2025-05-15 12:08:36 +0300

    Visible Install only if available

--- LuminaryVisuals/wwwroot/functions.js
+++ LuminaryVisuals/wwwroot/functions.js
@@ -169,9 +169,4 @@ window.triggerInstallPrompt = function () {
     }
 };
 
-// Check for PWA support
-window.checkPwaSupport = function () {
-    if (!('beforeinstallprompt' in window)) {
-        alert("This browser doesn't support PWA installation.\nif you are on ios, you have to click share and then add to home manually for the app to be installed");
-    }
-};
+
